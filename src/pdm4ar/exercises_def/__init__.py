9a3ae43c407554563aea84d7f8c1bd0ed3f15e28
Anthonyeid11 <115183101+Anthonyeid11@users.noreply.github.com>
2022-10-14 16:52:44 +0200

    Initial commit

---
+++ src/pdm4ar/exercises_def/__init__.py
@@ -0,0 +1,11 @@
+from zuper_commons.logs import ZLogger
+
+logger = ZLogger(__name__)
+
+from .structures import *
+from .ex01 import *
+from .ex02 import *
+from .ex03 import *
+from .ex04 import *
+from .ex06 import *
+from .ex07 import *
